a6c5e1fb3db75a4678616843110a0b8bb9d1e6bf
Austin Lee <austinlee@usf.edu>
2025-09-09 11:42:24 -0400

    squiggly brackets

--- cc_3.js
+++ cc_3.js
@@ -1,8 +1,8 @@
-let customers=
+let customers={
     let Names=['Tim','Dim','Zim'];
     let CustomerEmail=['t@mail','Dim@mail','Zm@mail'];
     let Purchase=['Rock','table','pen'];
- [];
+};
 console.log(customers);
 customersName.push('Kim');
 customerNames.shift(1);
