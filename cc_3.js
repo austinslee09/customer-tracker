f4d2c9a82d41b6fce499a4419b16df4b27f0e3d0
Austin Lee <austinlee@usf.edu>
2025-09-09 10:59:24 -0400

    camelcase

--- cc_3.js
+++ cc_3.js
@@ -1,8 +1,8 @@
- Customers=[
-    let CustomerNames=["Tim","Dim","Zim"];
+ customers=[];
+    let customerNames=["Tim","Dim","Zim"];
     let CustomerEmail=["t@mail","Dim@mail","Zm@mail"];
     let Purchase=["Rock","table","pen"];
- ];
+ 
 console.log(customers);
 customersName.push("Kim");
 customers.shift(1);
