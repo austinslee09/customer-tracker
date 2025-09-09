dc8ac56f97ba6c288ab6132f3c091fc4293b64a3
Austin Lee <austinlee@usf.edu>
2025-09-09 10:56:11 -0400

    syntax error

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,8 @@
- Customers=[
-    let Names=["Tim","Dim","Zim"]
-    let CustomerEmail=["t@mail", "Dim@mail", "Zm@mail"]
-    let Purchase=["Rock","table","pen"]
-]
-console.log customers.push("Kim")
-customers.shift(1)
+ Customers=[];
+    let CustomerNames=["Tim","Dim","Zim"];
+    let CustomerEmail=["t@mail","Dim@mail","Zm@mail"];
+    let Purchase=["Rock","table","pen"];
+
+console.log(customers);
+customers.push("Kim");
+customers.shift(1);
