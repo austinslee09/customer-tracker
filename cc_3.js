bf67fa3bb573e0c4c20ae232cb84ddbf30076411
Austin Lee <austinlee@usf.edu>
2025-09-09 15:19:42 -0400

    syntax

--- cc_3.js
+++ cc_3.js
@@ -4,6 +4,6 @@ let customers={
     Purchase:['Rock','table','pen']
 };
 console.log(customers);
-customers.Names.push('Kim');
-customers.Names.shift(1);
+Names.push("Kim");
+Names.shift(1);
 console.log(customers)
