7cab431e23d6443b105ecb5f7bde1acdb32f884a
Austin Lee <austinlee@usf.edu>
2025-09-09 11:50:07 -0400

    ; remove 2-4

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,7 @@
 let customers={
-    Names:['Tim','Dim','Zim'];
-    CustomerEmail:['t@mail','Dim@mail','Zm@mail'];
-    Purchase:['Rock','table','pen'];
+    Names:['Tim','Dim','Zim']
+    CustomerEmail:['t@mail','Dim@mail','Zm@mail']
+    Purchase:['Rock','table','pen']
 };
 console.log(customers);
 customersName.push('Kim');
