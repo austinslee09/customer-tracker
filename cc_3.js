5e3181d6f12d7e3299d88a5d2198dee46327ee9b
Austin Lee <austinlee@usf.edu>
2025-09-09 11:01:45 -0400

    " to '

--- cc_3.js
+++ cc_3.js
@@ -1,8 +1,8 @@
  customers=[
-    let customerNames=["Tim","Dim","Zim"];
-    let CustomerEmail=["t@mail","Dim@mail","Zm@mail"];
-    let Purchase=["Rock","table","pen"];
+    let customerNames=['Tim','Dim','Zim'];
+    let CustomerEmail=['t@mail','Dim@mail','Zm@mail'];
+    let Purchase=['Rock','table','pen'];
  ];
 console.log(customers);
-customersName.push("Kim");
+customersName.push('Kim');
 customerNames.shift(1);
