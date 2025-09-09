2ade9365747388be02b9de46045e2b138c55f07d
Austin Lee <austinlee@usf.edu>
2025-09-09 10:58:10 -0400

    brackets test ln5

--- cc_3.js
+++ cc_3.js
@@ -1,8 +1,8 @@
- Customers=[];
+ Customers=[
     let CustomerNames=["Tim","Dim","Zim"];
     let CustomerEmail=["t@mail","Dim@mail","Zm@mail"];
     let Purchase=["Rock","table","pen"];
-
+ ];
 console.log(customers);
 customersName.push("Kim");
 customers.shift(1);
