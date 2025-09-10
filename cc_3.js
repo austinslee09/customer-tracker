02bcf0482327584ba127aa58964588b0af66cd33
Austin Lee <austinlee@usf.edu>
2025-09-10 09:54:09 -0400

    ln9 and 10 push email and purchase

--- cc_3.js
+++ cc_3.js
@@ -6,6 +6,8 @@ let customers={
 console.log(customers);
 
 let extra= customers.Names.push("Kim");
+let mailing= customers.CustomerEmail.push("Kimm@mail");
+let but= customers.Purchase.push("bee");
 console.log(customers);
 
 let removeOne= customers.shift();
